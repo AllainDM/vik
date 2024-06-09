60f3cb3eb524b2f3139d77dcc3f573de4c89b0a6
allain <allaindemoupu@mail.ru>
2024-06-09 17:44:14 +0300

    Заглушка для боевки с рандомным уроном. Вывод потерь в лог.

--- war.py
+++ war.py
@@ -1,4 +1,6 @@
 # Модуль рассчета боя
+import random
+
 import maindb
 from FDataBase import FDataBase
 
@@ -27,11 +29,34 @@ def battle(game_id, inv_army, target_id):
     group_a = dbase404.get_all_our_units(game_id, 1, "army")
     print(f"units_group_a {group_a}")
     print("Выведем войска по циклу:")
-    units_group_a = []
+    units_group_a = []  # Список со словарями каждого юнита
+    # TODO сделать парамерт живой или мертвый для отметки в бд, чтобы не удалять
+    dead_id_units_in_group_a = set()  # Список с ид убитых юнитов(для удаления с бд)
     for u_group in group_a:
         for unit in u_group[1]:
             print(unit)
             units_group_a.append(unit)  # Обьединим всех юнитов всех групп в один список.
+    # Тестим два раунда боя, для правильного удаления юнитов.
+    # Не идем по циклу с конца, ибо не факт, что этот вариант подойдет для боевки
+    for round in range(1, 3):
+        print("######################")
+        print(f"Раунд: {round}")
+        for unit in units_group_a:
+            unit["hp_cur"] -= damage()
+            if unit["hp_cur"] <= 0:
+                dead_id_units_in_group_a.add(unit["row_id"])
+            print(unit)
+        units_group_a = [u for u in units_group_a if u["hp_cur"] > 0]
+        print("Юниты после раунда:")
+        print(units_group_a)
+    print("Потери:")
+    print(dead_id_units_in_group_a)
+    dead_in_group_a = f"Потери атакующей армии: {len(dead_id_units_in_group_a)}"
+    dead_in_group_b = f"Потери атакующей армии: Неизвестно"
 
-    return "Неизвестен."
+    return f"{dead_in_group_a}. {dead_in_group_b}"
+
+
+def damage():
+    return random.randint(0, 3)
 
